27adcceb5c84ed4e7879b456b569959bfcee15e8
Jonathan Farinloye <farinloyejonathan@gmail.com>
2021-03-01 18:20:43 +0100

    Postgres test on heroku

--- data.go
+++ data.go
@@ -3,20 +3,23 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func initDB() *gorm.DB {
-	dbName := "notes"
-	port := 5432
-	pass := "postgres"
-	user := "postgres"
-	host := "localhost"
-	ssl := "disable"
+	// dbName := "notes"
+	// port := 5432
+	// pass := "postgres"
+	// user := "postgres"
+	// host := "localhost"
+	// ssl := "disable"
 
-	connectionLink := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=%s", host, port, user, dbName, pass, ssl)
+	// connectionLink := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=%s", host, port, user, dbName, pass, ssl)
+
+	connectionLink := os.Getenv("DATABASE_URL")
 
 	db, err := gorm.Open(postgres.Open(connectionLink), &gorm.Config{})
 	log.Println(err)
