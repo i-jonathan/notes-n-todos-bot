31509a7ac94e8a042f64dae2024c99f81ce58ff3
Jonathan Farinloye <farinloyejonathan@gmail.com>
2021-03-02 00:58:48 +0100

    fix bad request on help text

--- functions.go
+++ functions.go
@@ -34,7 +34,7 @@ func processRequest(body *webHookReqBody) {
 						"/donetask <pre>number(s)</pre> - Marks indicated Todo items as done." +
 						"Use the number displayed from /viewtodolist. For multiple numbers, separate them with a space.\n\n" +
 						"/viewtodolist - List all your items on your Todo list.\n\n\n" +
-						"<b>Note Commands:</b>\n/addnote <pre><title></pre> <pre><note content</pre>  - Adds a new note with title and content.\n\n" +
+						"<b>Note Commands:</b>\n/addnote <pre>title</pre> <pre>note content</pre>  - Adds a new note with title and content.\n\n" +
 						"/deletenote <pre>1 5</pre> - Deletes notes 1 and 5 (an example)\n" + 
 						"/listnotes - Lists all available notes.\n" +
 						"/readnote <pre>1</pre> - Reads note 1"
